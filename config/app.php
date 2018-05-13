48555763d9ef74b60260a6a616b307d3f72df8e6
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2018-05-12 21:33:13 -0400

    feat: hack around the fact that Configure::load() doesn't do a recursive merge

--- config/app.php
+++ config/app.php
@@ -1,41 +1,54 @@
 <?php
-return [
-    'Users' => [
-        // User model.
-        'userModel' => 'Users.Users',
-        // Enable users the ability to upload avatars
-        'enableAvatarUploads' => true,
-        // Enable the password-reset flow
-        'enablePasswordReset' => true,
+use Cake\Core\Configure;
+use Cake\Utility\Hash;
+
+$defaults = Configure::read('Users');
+
+$values = $defaults + [
+    // User model.
+    'userModel' => 'Users.Users',
+    // Enable users the ability to upload avatars
+    'enableAvatarUploads' => true,
+    // Enable the password-reset flow
+    'enablePasswordReset' => true,
+    // Finder conditions
+    'conditions' => Hash::merge(Hash::get($defaults, 'conditions', []), [
+        // Require that the user be "active"
+        'active' => null,
         // Require that a user's email be authenticated
-        'requireEmailAuthentication' => true,
-        // Make all users active immediately
-        'setActiveOnCreation' => true,
-        // Track last login timestamp in the database and session
-        'trackLoginActivity' => true,
-        // Track last activity timestamp in the database and session
-        'trackLastActivity' => true,
-        // Fields to use for authentication
-        'fields' => [
-            'username' => 'email',
-            'password' => 'password',
-        ],
-        // A route to the controller action that handles logins
-        'loginAction' => [
-            'plugin' => 'Users',
-            'prefix' => false,
-            'controller' => 'Users',
-            'action' => 'login'
-        ],
-        'loginRedirect' => null,
-        'logoutRedirect' => [
-            'plugin' => 'Users',
-            'prefix' => false,
-            'controller' => 'Users',
-            'action' => 'login',
-        ],
-        // Social configuration
-        'social' => [
-        ],
-    ],
+        'emailAuthenticated' => null,
+    ]),
+    // Make all users active immediately
+    'setActiveOnCreation' => true,
+    // Track last login timestamp in the database and session
+    'trackLoginActivity' => true,
+    // Track last activity timestamp in the database and session
+    'trackLastActivity' => true,
+    // Fields to use for authentication
+    'fields' => Hash::merge(Hash::get($defaults, 'fields', []), [
+        'username' => 'email',
+        'password' => 'password',
+    ]),
+    // A route to the controller action that handles logins
+    'loginAction' => Hash::merge(Hash::get($defaults, 'loginAction', []), [
+        'plugin' => 'Users',
+        'prefix' => false,
+        'controller' => 'Users',
+        'action' => 'login',
+    ]),
+    'loginRedirect' => Hash::merge(Hash::get($defaults, 'loginRedirect', []), [
+    ]),
+    'logoutRedirect' => Hash::merge(Hash::get($defaults, 'logoutRedirect', []), [
+        'plugin' => 'Users',
+        'prefix' => false,
+        'controller' => 'Users',
+        'action' => 'login',
+    ]),
+    // Social configuration
+    'social' => Hash::merge(Hash::get($defaults, 'social', []), [
+    ]),
+];
+
+return [
+    'Users' => $values,
 ];
